abfdde0fa0c897e27b414ecae18ae2f0c7ab0a2c
Natalie <natali14441@gmail.com>
2020-03-20 12:39:41 +0300

    feat: add common array functions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,33 @@
 
 exports.min = function min (array) {
-  return 0;
+    if (array && array.length) {
+        let min = array[0];
+        for (let i = 1; i < array.length; i++) {
+            min = array[i] < min ? array[i] : min;   
+        } 
+        return min;  
+    }
+    return 0;
 }
 
 exports.max = function max (array) {
-  return 0;
+    if (array && array.length) {
+        let max = array[0];
+        for (let i = 1; i < array.length; i++) {
+            max = array[i] > max ? array[i] : max;   
+        } 
+        return max;  
+    }
+    return 0;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+    if (array && array.length) {
+        let sum = 0;
+        for (let i = 0; i < array.length; i++) {
+            sum += array[i];
+        } 
+        return sum / array.length;  
+    }
+    return 0;
 }
